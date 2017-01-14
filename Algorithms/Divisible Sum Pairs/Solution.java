6a8f98575fa7a7c51e8899c1dbc0e871169e10e4
rfehr <rfehr@umail.iu.edu>
2017-01-14 15:29:29 -0500

    Changed Divisible Sum Pairs to run in O(n) time complexity instead of O(n^2)

--- Algorithms/Divisible Sum Pairs/Solution.java
+++ Algorithms/Divisible Sum Pairs/Solution.java
@@ -19,16 +19,32 @@ public class Solution {
         
         int validPairs = 0;
         
-        for(int i = 0; i < a.length; i++)
+        int[] modSort = new int[k];
+        
+        for(int num : a)
         {
-            for(int j = i+1; j < a.length; j++)
-            {
-                if((a[i]+a[j]) % k == 0)
-                {
-                    validPairs++;
-                }
-            }
+            int modIndex = num % k;
+            modSort[modIndex] += 1;
         }
+        
+        validPairs += (modSort[0]*(modSort[0]-1)/2);//Determines valid pairs of the values that had mod 0        
+        
+        for(int i = 1; i < k/2.0; i++)
+        {
+                //Determines two mod indices that when summed have a mod 0 index
+                int x = modSort[i];
+                int y = modSort[k-i];
+                
+                //Determine valid pairs of x + y integers
+                validPairs += x*y;
+        }
+        
+        if(k % 2 == 0)//Handles the edge case of calculating valid pairs for when the two mod indices are the same
+        {
+            validPairs += (modSort[k/2]*(modSort[k/2]-1)/2);
+        }
+        
         System.out.println(validPairs);
     }
 }
+
