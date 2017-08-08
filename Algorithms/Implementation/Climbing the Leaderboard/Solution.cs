ba7ef583d6ba0c6fda2c39d92b53827ce1f2a2bc
Rasik Bihari Tiwari <rasik210@gmail.com>
2017-08-08 17:05:11 +0530

    Update Solution.cs

--- Algorithms/Implementation/Climbing the Leaderboard/Solution.cs
+++ Algorithms/Implementation/Climbing the Leaderboard/Solution.cs
@@ -3,25 +3,25 @@
  C# Language Version: 6.0
  .Net Framework Version: 4.5.2
  Thoughts :
- 1. Get an array of all the player scores. Iterate through all player scores and create a new array 
+ 1. Get an array of n player scores. Iterate through all player scores and create a new array 
     which doesn't contain any repeated player scores. Let us call the new array, playerScores.
- 2. Let total levels to be played by Alice is N.
+ 2. Let total levels to be played by Alice is m.
  3. Let Alice's score after first round be S.
  4. Let Alice's initial rank R be 0.
  5. Start iterating playerScores array from the rear end untill you get a player score whose score is less than S.
  6. Set R to rank of the player found in step 5.
- 7. Reduce N by 1.
+ 7. Reduce m by 1.
  8. Print R.
  9. Now start processing Alice's score in all subsequent levels inside a loop
     9.1 Set S to Alice's next level score.
     9.2 Start iterating playerScores array from the player whose rank is R-1 towards the front end.
     9.3 Continue iteration untill you get a player whose score is less than S.
     9.4 Set R to rank of the player found in above step.
-    9.5 Reduce N by 1.
+    9.5 Reduce m by 1.
     9.6 Print R.
-    9.7 Go to step 9.1 if there are more levels left to be played (i. e N>0).
+    9.7 Go to step 9.1 if there are more levels left to be played (i. e m>0).
 
- Time Complexity:  O(n) - One scan required to get non-duplicate scores (n). Another scan required to decide
+ Time Complexity:  O(n+m) - One scan required to get non-duplicate scores (n). m iterations required to decide
                         Alice's rank after each level.
  Space Complexity: O(n) - An additional array for entire input set is required to store non-duplicate scores.
         
