faaf225e868eb668e9bb1d762b4e171b7cbf6236
Ryan Fehr <rfehr@umail.iu.edu>
2017-09-21 22:12:22 -0400

    Added solution to Java problem called Java Substring

---
+++ Java/Java Substring/Solution.java
@@ -0,0 +1,18 @@
+// Problem: 
+// Java 8
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
