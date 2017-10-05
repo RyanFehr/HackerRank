3556c185bf8272883a6e2d65d0925c1173ac29d6
jastee07 <jake.steele54@gmail.com>
2017-10-05 16:10:43 -0500

    Added Solution.py

---
+++ Algorithms/Warmup/Solve Me First/Solution.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/solve-me-first
+
+
+def solve_me_first(a, b):
+
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solve_me_first(num1, num2)
+print(res)
