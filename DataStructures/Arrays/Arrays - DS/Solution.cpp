2b88d9c37cb632346776cb0d11e28bc6423729e5
Ryan Fehr <rfehr@umail.iu.edu>
2018-02-02 16:19:55 -0500

    Updated malloc to have an explicit cast
    Added explicit static cast from void* to int* instead of implicit cast

--- DataStructures/Arrays/Arrays - DS/Solution.cpp
+++ DataStructures/Arrays/Arrays - DS/Solution.cpp
@@ -18,7 +18,7 @@
 int main(){
     int n; 
     scanf("%d",&n);
-    int *arr = malloc(sizeof(int) * n);
+    int *arr = static_cast<int*>(malloc(sizeof(int) * n));
     for(int arr_i = 0; arr_i < n; arr_i++){
        scanf("%d",&arr[arr_i]);
     }
