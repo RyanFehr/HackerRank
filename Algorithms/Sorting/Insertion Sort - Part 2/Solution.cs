d1b7a04a4d4f3739032a06cfaebaf3d2bbae35c6
Rasik Bihari Tiwari <rasik210@gmail.com>
2018-02-03 16:56:08 +0530

    Improvised the algorithm.
    Earlier I was using two seprate loops one each for finding the insertion position and then other one for shifting the elements. Now I've achieved the entire stuff using only one loop.

--- Algorithms/Sorting/Insertion Sort - Part 2/Solution.cs
+++ Algorithms/Sorting/Insertion Sort - Part 2/Solution.cs
@@ -8,57 +8,45 @@
          2. Initialize a counter c with 1.
          3. Start a loop until c < n
              3.1 Let the element to be sorted in current pass is e located at cth index of arr.
-             3.2 Initialize a counter j = 0.
-             3.3 Start a loop to obtain the position where e actually needs to be inserted in the array.
-                 3.3.1 if j < c then continue the loop else break it and go to step 3.4
-                 3.3.2 if e <= arr[j] then break the loop and go to step 3.4
-                 3.3.3 increment j by 1.
+             3.2 Initialize a counter j = c-1.
+             3.3 Start a loop 
+                 3.3.1 if e < arr[j] then set arr[j + 1] = arr[j].
+                 3.3.2 if e >= arr[j] then break the loop.
+                 3.3.3 decrement j by 1.
                  3.3.4 Keep repeating steps from 3.3.1 through 3.3.3 untill loop breaking condition isn't met.
-             3.4 Now j holds the position where e actually needs to be inserted.
-             3.5 Initialize a new counter i and set it to c. Now start a loop for shifting elements towards right to create the void 
-                 at position j for e to fill in:
-                 3.5.1 If i <= j break the loop and go to step 3.6
-                 3.5.2 set arr[i] = arr[i - 1]
-                 3.5.3 decrement i by 1.
-                 3.5.4 Keep repeating steps from 3.5.1 through 3.5.3 untill loop breaking condition isn't met.
-             3.6 Replace jth element of array with e.
-             3.7 Print the entire array elements separated by space character.
-             3.8 Increment c by 1.
-             3.9 Keep repeating steps from 3.1 through 3.8 untill loop breaking condition isn't met.
+             3.4 j+1 is the correct position where e needs to be inserted. Replace (j+1)th element of array with e.
+             3.5 Increment c by 1.
+             3.6 Keep repeating steps from 3.1 through 3.5 untill loop breaking condition isn't met.
 
          Time Complexity:  O(n^2) //There is a nested loop.
          Space Complexity: O(1) //number of dynamically allocated variables remain constant for any input.
-         */
+                
+        */
 using System;
 
 class Solution
 {
+
     static void Insert(int[] arr, int i)
     {
         var currElement = arr[i];
-        var j = 0;
-        for (; j < i; j++)
+        var j = i - 1;
+        for (; j >= 0; j--)
         {
-            if (currElement <= arr[j])
+            if (currElement < arr[j])
+                arr[j + 1] = arr[j];
+            else
                 break;
         }
-        //got the position where new element needs to be inserted - j
-
-        // from i to j index shift the elements to right tracking backwards towards left end of the array.
-        while (i > j)
-        {
-            arr[i] = arr[i - 1];
-            i--;
-        }
-        arr[j] = currElement;
+        arr[j + 1] = currElement;
     }
 
     static void Main(String[] args)
     {
-        //No need to capture the size of array. I use array's length property instead.
+        //No need to capture the size of array. We use array's length property instead.
         Console.ReadLine();
         var arr_temp = Console.ReadLine().Split(' ');
-        var arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        var arr = Array.ConvertAll(arr_temp, int.Parse);
         for (var i = 1; i < arr.Length; i++)
         {
             Insert(arr, i);
