0497272135db549acbb8ccd10f77148a6f6adaaa
Rasik Bihari Tiwari <rasik210@gmail.com>
2018-05-08 18:05:22 +0530

    simplified the solution using string builder

--- Algorithms/Strings/Super Reduced String/Solution.cs
+++ Algorithms/Strings/Super Reduced String/Solution.cs
@@ -1,44 +1,40 @@
 /*
-        Problem: https://www.hackerrank.com/challenges/reduced-string/problem
-        C# Language Version: 6.0
-        .Net Framework Version: 4.7
-        Tool Version : Visual Studio Community 2017
-        Thoughts :
+         Problem: https://www.hackerrank.com/challenges/reduced-string/problem
+         C# Language Version: 6.0
+         .Net Framework Version: 4.7
+         Tool Version : Visual Studio Community 2017
+         Thoughts :
+         
+         We need to iterate the entire string and process it as per below mentioned steps:
+          - Append every character coming in the input sequence into a string builder if it is not matching with the
+            last character in the string builder.
+          - If the character in input sequence matches with last character in the string builder then remove the last character
+            from the string builder.
 
-        We need to iterate the entire string and process it as per below mentioned steps:
-         - remove the repeated characters whereever found and then process the remaining string from the point where
-           repeated character was found.
-         - special handling : Consider the string "abccbee". Here when we remove the pair "cc" then we need to start scanning
-           the string from position of first occurence of b else "bb" pair will not be caught.
-         - Some special handling required when repeated sequences occur at the beginning of the string
-
-        Time Complexity:  O(n) //we need to iterate the entire string of length n once to remove all the repeated characters.
-        Space Complexity: O(n) //we need to store the input string in memory to process it and find repeated characters.
-
-       */
+         Time Complexity:  O(n) //we need to iterate the entire string of length n once to remove all the repeated characters.
+         Space Complexity: O(n) //we need to store the input string in memory to process it and find repeated characters.
+         
+        */
 
 using System;
+using System.Text;
 
 class Solution
 {
     static string SuperReducedString(string sequence)
     {
-        while (sequence.Length > 0 && sequence[0] == sequence[1])
-            sequence = sequence.Substring(2);
+        var sb = new StringBuilder();
+        sb.Append(sequence[0]);
 
-        for (var i = 0; i < sequence.Length - 1;)
+        for (var i = 1; i < sequence.Length; i++)
         {
-            if (sequence[i] == sequence[i + 1])
-            {
-                var part1 = sequence.Substring(0, i);
-                var part2 = sequence.Substring(i + 2);
-                sequence = part1 + part2;
-                i = i > 0 ? i - 1 : i;
-                continue;
-            }
-            i++;
+            if (sb.Length > 0 && sequence[i] == sb[sb.Length - 1])
+                sb = sb.Remove(sb.Length - 1, 1);
+            else
+                sb.Append(sequence[i]);
+
         }
-        return sequence;
+        return sb.ToString();
     }
 
     static void Main(String[] args)
