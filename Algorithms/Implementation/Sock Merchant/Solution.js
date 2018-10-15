375085825844be4d9da4770c94a7ed1698994efb
KushalGH <kushalseth1408@gmail.com>
2018-10-15 23:29:43 +0530

    Implemented Sock Merchant Porblem in Javascript

---
+++ Algorithms/Implementation/Sock Merchant/Solution.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+
+    ar = ar.sort();
+    var count = 0;
+    if(ar && ar.length > 0) {
+        for(var i = 0; i < ar.length; i++) {
+            if(i < (ar.length - 1) && ar[i] == ar[i + 1]) {
+                count++;
+                i++;
+            }
+        }        
+    }
+    else {
+        
+    }
+
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
