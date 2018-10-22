b17a12c1e56f3b083983fa1c42852ce9eab924e8
Cristóvão L. Frinhani <cristovao.frinhani@gmail.com>
2018-10-22 19:56:30 -0300

    Update Solution.py

--- Algorithms/Warmup/Solve Me First/Solution.py
+++ Algorithms/Warmup/Solve Me First/Solution.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-//Problem: https://www.hackerrank.com/challenges/solve-me-first
+#Problem: https://www.hackerrank.com/challenges/solve-me-first
 a = int(input())
 b = int(input())
 print (a+b)
