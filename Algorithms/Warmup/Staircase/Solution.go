9534188d7565d9db27e28d83b0e29d4899e70016
jwalter1-quest <jeff.walter@quest.com>
2019-01-28 19:54:45 -0600

    Go Solution: Warmup/Staircase

---
+++ Algorithms/Warmup/Staircase/Solution.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the staircase function below.
+func staircase(n int32) {
+    var i int32
+    for i=1; i <= n; i++ {
+        spaces := strings.Repeat(" ", int(n - i)) 
+        hashes := strings.Repeat("#", int(i))
+        fmt.Println(spaces + hashes)
+    }
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
