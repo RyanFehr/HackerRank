76687d21992553be0c15b90923b09a3c47b9cf4a
medifle <13296643+medifle@users.noreply.github.com>
2019-03-17 00:22:18 -0400

    Add time and space complexity

--- DataStructures/Arrays/Left Rotation/Solution.java
+++ DataStructures/Arrays/Left Rotation/Solution.java
@@ -5,11 +5,15 @@ Java 8
 Initial thoughts
 We can remove the first element, shift the rest elements left by 1 position,
 then add the removed element to the end
+Time Complexity: O(n^2)
+Space Complexity: O(n)
 
 Optimization
 Modification to the array is not really necessary.
 We print the array starting from the index d
 If bound is hit, we use modulo operation to go back to index 0
+Time Complexity: O(n)
+Space Complexity: O(n)
 
  */
 
