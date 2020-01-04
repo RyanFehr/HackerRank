e7eddfce6b530c746b4a13e5cd363350e574ddb1
jilsonjoseph <36656350+jilsonjoseph@users.noreply.github.com>
2020-01-04 14:51:40 +0530

    Update Electronic Shop/Solution.java
    if max has reached the  maximum possible value that is max == s, then there is no need to continue the loop

--- Algorithms/Implementation/Electronics Shop/Solution.java
+++ Algorithms/Implementation/Electronics Shop/Solution.java
@@ -42,11 +42,13 @@ public class Solution {
         Arrays.sort(pendrives);//Ascending order
         
         int max = -1;
-        for(int i = 0, j = 0; i < n; i++){
+        outer: for(int i = 0, j = 0; i < n; i++){
             for(; j < m; j++){
                 if(keyboards[i]+pendrives[j] > s) break; //This prevents j from incrementing
                 if(keyboards[i]+pendrives[j] > max)
                     max = keyboards[i]+pendrives[j];
+                    if(max == s)
+                        break outer;
             }
         }
         System.out.println(max);
