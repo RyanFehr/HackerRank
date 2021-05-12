b95c878aaed7e14ecc4c23939119e547a4f4233b
Dakshitha Dissanayaka <45692186+Dakshitha96@users.noreply.github.com>
2021-05-12 12:49:33 +0530

    Create Solution2.java

---
+++ Java/Java Loops I/Solution2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        for (int i=1; i<=10; i++){
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+
+        bufferedReader.close();
+    }
+}
