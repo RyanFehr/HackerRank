a82fb921be58f0743da294e782ff38ddf4c42f46
Nawin Kumar Sharma <126372969+NawinKumarSharma@users.noreply.github.com>
2023-03-23 09:48:12 +0530

    Update Solution.java

--- Java/Java Loops II/Solution.java
+++ Java/Java Loops II/Solution.java
@@ -33,7 +33,7 @@ import java.io.*;
 import java.lang.Math.*;
 
 class Solution{
-    public static void main(String []argh){
+    public static void main(String []args){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
